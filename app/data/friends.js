02a74c7c828f669d89b0ad1755d1a1f266002e1e
nise406 <44655381+nise406@users.noreply.github.com>
2019-02-26 18:26:43 -0800

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Ursula',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwih446svdrgAhWb_YMKHXItDL0QjRx6BAgBEAU&url=https%3A%2F%2Fwww.facebook.com%2FDisneyUrsula%2F&psig=AOvVaw3070PsUbBl_QM7LRZuJ5a4&ust=1551307456722132',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Gaston',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj325mavdrgAhUZ8YMKHdkSD0YQjRx6BAgBEAU&url=https%3A%2F%2Fmedium.com%2F%40JeremyWh%2Fgaston-isnt-big-enough-e6ae199bb191&psig=AOvVaw0PfEf1-BxbodVbs6kszrgJ&ust=1551307544195768',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Bruce',
+		photo: 'https://paladone.com/blog/2017/07/13/shark-week-2017/finding_nemo_fanfiction___breaking_bruce_by_jesus_ish-d9u20xy/',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Scar',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjH4OPKvtrgAhUlw4MKHckcDOkQjRx6BAgBEAU&url=https%3A%2F%2Fmedium.com%2F%40haldash%2Fscar-from-disneys-the-lion-king-was-actually-the-film-s-hero-b9505e965f4b&psig=AOvVaw2cRo3woYT-7W6x0fOEtGcj&ust=1551307920375302',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Hades',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjp3e2Av9rgAhWC_YMKHVrLD9cQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F529665606153257585%2F&psig=AOvVaw1Ca50-O2X40wPeuAwWaQJb&ust=1551308057270755',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Jafar',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiVqImYv9rgAhUMw4MKHc6hBhUQjRx6BAgBEAU&url=https%3A%2F%2Fhellogiggles.com%2Fnews%2Fhot-jafar-live-action-disney-aladdin%2F&psig=AOvVaw3Xcne_aSWnSEuikgAFPGMS&ust=1551308105959543',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Governor Ratcliffe',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj6hKe2v9rgAhVK54MKHYMVCFEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.deviantart.com%2Fjimma1300%2Fart%2FVillains-as-heroes-Governor-Ratcliffe-601363577&psig=AOvVaw2kYhbmKfWP1i9A6vJ0ThnF&ust=1551308149518131',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Shere Khan',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiy2bnfv9rgAhVH5IMKHcCJDWUQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FShere_Khan&psig=AOvVaw3VOomGyOz3_BkaWwHglgIz&ust=1551308254733813',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Yzma',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjHnuGQwNrgAhVFw4MKHXs3DvEQjRx6BAgBEAU&url=https%3A%2F%2Fevilbabes.fandom.com%2Fwiki%2FYzma_(The_Emperor%2527s_New_Groove)&psig=AOvVaw1LJr47fTaZ3PCY7ezwk6YG&ust=1551308328043154',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Frollo',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj28IqvwNrgAhWHyIMKHSrMAVUQjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Fhellfirefrollo&psig=AOvVaw14mDGnT5agN-oIjUWxjGE3&ust=1551308422898234',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
